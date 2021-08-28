21a5b43fa721fb962a62be59d0037e70f4727494
windows 7 <windows 7@windows7-PC>
2021-08-29 00:33:00 +0530

    added 2 classes

---
+++ src/test/java/istiyak/A.java
@@ -0,0 +1,10 @@
+package istiyak;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("dhedh");
+	}
+
+}
---
+++ src/test/java/istiyak/Bfgf.java
@@ -0,0 +1,10 @@
+package istiyak;
+
+public class Bfgf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("gcdhdhdh");
+	}
+
+}
